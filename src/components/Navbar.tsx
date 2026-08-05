6af803a96f17087d219cb1f8fae9af33408acd46
huxlic <oladimejihassan03@gmail.com>
2026-08-05 21:47:29 +0100

    refactor Navbar component for improved readability and update dependencies for GSAP

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,11 @@ import {navLinks} from "../shared/navLinks.ts";
 import type {NavLinks} from "../types";
 
 export const Navbar = () => {
+	
 	return (
 		<>
-			<header className={"fixed top-0 w-full left-0 right-0 box-border px-8 bg-linear-to-t from-transparent to-[#0D0F11] z-50"}>
+			<header
+				className={"fixed top-0 w-full left-0 right-0 box-border px-8 bg-linear-to-t from-transparent to-[#0D0F11] z-50"}>
 				<nav className={"flex items-center justify-between w-full py-4"}>
 					<Link to="/"
 					      className={"w-max h-max font-long-cang text-xl mask-b-from-50% text-wuxia flex items-center gap-1 uppercase font-bold"}>
@@ -19,16 +21,16 @@ export const Navbar = () => {
 					
 					<ul className={"flex items-center font-marcellus gap-6"}>
 						{navLinks.map(({href, title, icon: Icon}: NavLinks) => (
-							<li key={title} className={"flex items-center text-[#ffffff99] hover:text-wuxia text-[13px] transition-colors duration-300 gap-1"} >
-								<NavLink className={({isActive}) => isActive ? "text-wuxia" : ""} to={href}>{title}</NavLink>
-								{Icon && <Icon size={18}/>}
-							</li>
-						))}
+							<li key={title}
+							    className={"text-[#ffffff99] hover:text-wuxia text-[12px] transition-colors duration-300 gap-1"}>
+								<NavLink
+									className={({isActive}) => isActive ? "text-wuxia flex items-center " : "flex items-center "}
+									to={href}>{title} {Icon && <Icon size={18}/>}</NavLink>
+							</li>))}
 					</ul>
 					
 					<Link to={"/"}><img draggable={false} className={"w-8"} src={samurai} alt=""/></Link>
 				</nav>
 			</header>
-		</>
-	);
+		</>);
 };
