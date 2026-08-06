356e659aacd1eaf7c1a0990000d57f86f255c323
huxlic <oladimejihassan03@gmail.com>
2026-08-06 14:19:07 +0100

    feat:enhance Homepage with 3D Samurai model and buy now button; update dependencies for react-three

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,11 @@
 import {Navbar} from "../components/Navbar.tsx";
 import landscape from "../assets/images/landscape.jpg";
-import samurai_warrior from "../assets/videos/samurai-warrior.webm"
+import buy_now from "../assets/images/buy-now.png";
+import {Link} from "react-router";
+import {Canvas} from "@react-three/fiber";
+import Samurai from "../components/models/Samurai.tsx"
+import {Center, OrbitControls} from "@react-three/drei";
+import {Suspense} from "react";
 
 const Homepage = () => {
 	return (
@@ -8,12 +13,30 @@ const Homepage = () => {
 			<div className={"relative"}>
 				<Navbar/>
 				
-				<section className={"relative h-screen overflow-hidden"} >
-					<div className="absolute inset-0 bg-cover blur-[2px]" style={{backgroundImage: `url(${landscape})`}}></div>
-					<video autoPlay={true} muted={true} loop={true} playsInline={true} className={"absolute bottom-0 left-1/2 -translate-x-1/2 h-[90%]"}>
-						<source src={samurai_warrior} />
-					</video>
+				<section className={"relative h-screen overflow-hidden flex items-center justify-center"}>
+					<div className="absolute inset-0 bg-cover blur-[2px]"
+					     style={{backgroundImage: `url(${landscape})`}}></div>
+					<div className="bg-[#000000cc] absolute inset-0"></div>
+					
+					<div className="absolute inset-0">
+						<Canvas id="canvas" camera={{position: [0, 2, 5], fov: 75, near: 1, far: 1000}}>
+							<ambientLight intensity={0.6}/>
+							<directionalLight position={[3, 5, 2]} intensity={1.5}/>
+							<Suspense fallback={null}>
+								<Center>
+									<Samurai scale={0.08} position={[0, 8, 0]}/>
+								</Center>
+							</Suspense>
+							<OrbitControls autoRotate={true} enableZoom={false} minPolarAngle={Math.PI / 2}
+							               maxPolarAngle={Math.PI / 2}/>
+						</Canvas>
+					</div>
+					
+					<Link className={"absolute bottom-0"} to={"/"}>
+						<img draggable={false} src={buy_now} className={"w-50"} alt="a gold buy now cta"/>
+					</Link>
 				</section>
+			
 			</div>
 		</>
 	)
