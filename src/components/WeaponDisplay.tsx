9a7567475e62cdc96fa615c1192f5bcf6f451dfb
huxlic <oladimejihassan03@gmail.com>
2026-08-11 19:15:58 +0100

    feat: enhance WeaponDisplay component with GSAP animations and refactor model positioning

--- src/components/WeaponDisplay.tsx
+++ src/components/WeaponDisplay.tsx
@@ -1,10 +1,30 @@
 import {Environment, OrbitControls} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
-import {type ComponentType} from "react";
+import {type ComponentType, useRef} from "react";
+import gsap from "gsap";
+import * as THREE from "three";
+import {useGSAP} from "@gsap/react";
 
 export const WeaponDisplay = ({ModelComponent}: {
 	ModelComponent: ComponentType<{ position?: [number, number, number]; rotation?: [number, number, number] }>
 }) => {
+	
+	const groupRef = useRef<THREE.Group>(null);
+	const finalPosition: [number, number, number] = [-1, 0, -1];
+	
+	useGSAP(() => {
+		if (!groupRef.current) return;
+		
+		gsap.fromTo(
+			groupRef.current.position,
+			{ x: finalPosition[0] + 4 },
+			{
+				x: finalPosition[0],
+				duration: 1,
+				ease: "power3.out",
+			}
+		);
+	}, [ModelComponent]);
 	
 	return (
 		<>
@@ -13,11 +33,14 @@ export const WeaponDisplay = ({ModelComponent}: {
 					<ambientLight intensity={0.25}/>
 					<directionalLight position={[3, 5, 2]} intensity={1.8}/>
 					
-					<ModelComponent position={[-1, 0, -1]} rotation={[0, 0, Math.PI / 4]}/>
+					<group ref={groupRef} position={finalPosition}>
+						<ModelComponent rotation={[0, 0, Math.PI / 4]}/>
+					</group>
 					
 					<Environment preset="night"/>
 					<OrbitControls
-						enableZoom={true}
+						target={finalPosition}
+						enableZoom={false}
 						enablePan={false}
 					/>
 				</Canvas>
