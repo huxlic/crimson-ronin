e4b9d7ea5db8298a79d65eb415e6ed13f43064c3
huxlic <oladimejihassan03@gmail.com>
2026-08-16 16:19:40 +0100

    feat: create Characters component and update Loader import paths for consistency

---
+++ src/components/sections/Characters.tsx
@@ -0,0 +1,9 @@
+export const Characters = () => {
+	return (
+		<>
+			<section>
+			
+			</section>
+		</>
+	);
+};
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,5 +1,5 @@
 import {Html} from "@react-three/drei";
-import {GlassCube} from "./components/ui/GlassCube.tsx";
+import {GlassCube} from "./GlassCube.tsx";
 
 export const Loader = () => {
 	return (
